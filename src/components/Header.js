974f0c218bbb49cea68e153ab0533fba93b98176
hrithikapps <hrithikk2765@gmail.com>
2025-02-15 13:14:19 +0530

    feat: CSS fixes and added  hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,12 @@ import { useSelector } from "react-redux";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { CLIENT_ID } from "./Constants";
 import jwt_decode from "jwt-decode";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Title = () => {
   return (
-    <a className="h-20 pl-10" href="/">
-      {<img className="h-20 p-2 " src={Logo} alt="Logo" />}
+   <a className="h-20 flex items-center pl-10" href="/">
+      <img className="h-20 w-auto p-2" src={Logo} alt="Logo" />
     </a>
   );
 };
@@ -21,6 +22,7 @@ const Title = () => {
 // };
 const Header = () => {
   const [user, setUser] = useState({});
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLogout = () => {
     setUser({});
@@ -29,56 +31,74 @@ const Header = () => {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const isOnline = useOnline();
-
   const cartItems = useSelector((store) => store.cart.items);
   console.log(cartItems);
 
   return (
-    <div className="flex justify-between items-center pr-16 bg-slate-100">
+<div className="flex items-center px-10 py-2 bg-slate-100 relative">
       <Title />
-      <ul className="flex gap-5">
-        <li>
+
+      {/* Mobile Menu Toggle Button */}
+      <button
+        className="md:hidden text-2xl"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+
+      <ul
+        className={`absolute md:flex md:justify-center md:items-center md:static top-full left-0 w-full bg-slate-100 p-4 md:p-0 md:gap-4 text-lg transition-all duration-300 ease-in-out ${
+          menuOpen ? "block" : "hidden md:flex"
+        }`}
+      >
+        <li className="py-2 md:py-0 text-center md:px-2">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="py-2 md:py-0 text-center md:px-2">
           <Link to="/about">About</Link>
         </li>
-        <li>
+        <li className="py-2 md:py-0 text-center md:px-2">
           <Link to="/contactUs">Contact Us</Link>
         </li>
-        <li>
+        <li className="py-2 md:py-0 text-center md:px-2">
           <Link to="/cart">
-            Cart{" "}
-            <sup className=" text-white pl-2 pt-0.5 pb-0.5  pr-2 bg-orange-600 rounded-full text-base ">
-              {cartItems.reduce((acc, item) => {
-                return acc + item.qty;
-              }, 0)}
-            </sup>{" "}
+            Cart
+            <sup className="text-white pl-2 pt-0.5 pb-0.5 pr-2 bg-orange-600 rounded-full text-base">
+              {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+            </sup>
           </Link>
         </li>
       </ul>
-      {/* {isLoggedIn === true ? (
-        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
-      ) : (
-        <button onClick={() => setIsLoggedIn(true)}>Login</button>
-      )} */}
-      {Object.keys(user).length != 0 && (
-        <button onClick={() => handleLogout()}>Logout</button>
-      )}
-      {Object.keys(user).length == 0 && (
-        <GoogleOAuthProvider clientId={CLIENT_ID}>
-          <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              var decodedUserObject = jwt_decode(credentialResponse.credential);
-              setUser(decodedUserObject);
-              console.log(decodedUserObject);
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-        </GoogleOAuthProvider>
-      )}
+
+      {/* Google Authentication */}
+      <div className="hidden md:block">
+        {/* {isLoggedIn === true ? (
+          <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+        ) : (
+          <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        )} */}
+        {Object.keys(user).length !== 0 && (
+          <button className="text-lg" onClick={() => handleLogout()}>
+            Logout
+          </button>
+        )}
+        {Object.keys(user).length === 0 && (
+          <GoogleOAuthProvider clientId={CLIENT_ID}>
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                var decodedUserObject = jwt_decode(
+                  credentialResponse.credential
+                );
+                setUser(decodedUserObject);
+                console.log(decodedUserObject);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </GoogleOAuthProvider>
+        )}
+      </div>
     </div>
   );
 };
